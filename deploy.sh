74c171d1bdabc5372b2548fee890e9482e42442a
Abdulrhman Almadani <apo.1413@gmail.com>
2021-03-31 23:56:44 +0300

    refactor: cicd

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-#sshpass -p "$1" scp  -o "StrictHostKeyChecking no" ../backend.tgz root@164.90.165.230:/root/wtd
-#sshpass -p "$1" ssh -o "StrictHostKeyChecking no" root@164.90.165.230 "cd wtd && \
-#docker-compose down && \
-#docker rmi wtd_backend && \
-#rm -rf backend && \
-#tar -xvf backend.tgz && \
-#rm backend.tgz && \
-#docker-compose up -d"
-sshpass -p "$1" ssh -o "StrictHostKeyChecking no" root@164.90.165.230 "ls"
+sshpass -p "$1" scp  -o "StrictHostKeyChecking no" ../backend.tgz root@164.90.165.230:/root/wtd
+sshpass -p "$1" ssh -o "StrictHostKeyChecking no" root@164.90.165.230 "cd wtd && \
+docker-compose down && \
+docker rmi wtd_backend && \
+rm -rf backend && \
+tar -xvf backend.tgz && \
+rm backend.tgz && \
+docker-compose up -d"
